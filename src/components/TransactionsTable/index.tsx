22cef577c81eab576f827080f9b819cc9645ae90
Caio Lucas <lienscarlet@gmail.com>
2021-03-16 19:57:11 -0300

    feat $components: transactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
+
+export const TransactionsTable: React.FC = () => {
+  useEffect(() => {
+    async function loadTransactions() {
+      const response = await api.get('transactions');
+      console.log(response.data);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1.100,00</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>      
+    </Container>
+  )
+}
